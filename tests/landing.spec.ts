ddee2ec2d4c86ae62ce1520a6bc6687f1ead6f70
Mykola Petrov <mpetrov@lemon.io>
2023-03-01 18:31:47 +0100

    expect

--- tests/landing.spec.ts
+++ tests/landing.spec.ts
@@ -9,12 +9,16 @@ test('Add data to the fields', async ({ page }) => {
     const elementsTextBoxPageObject = new ElementsTextBoxPageObject(page);
 
     await elementsTextBoxPageObject.goto();
+    await elementsTextBoxPageObject.textBox();
+    
     await elementsTextBoxPageObject.fullName("John Galt");
-    await elementsTextBoxPageObject.email("example.com");
+    await elementsTextBoxPageObject.email("example@example.com");
     await elementsTextBoxPageObject.currentaddress("Ukraine, Kharkiv, Valentinivska 25a");
     await elementsTextBoxPageObject.permanentAddress("Albania, Vlore, Ruga Gjergj Kastrioti")
     await elementsTextBoxPageObject.submit();
 
-   
-
+    await expect(page.getByText('Name:John Galt')).toBeVisible();
+    await expect(page.getByText('Email:example@example.com')).toBeVisible();
+    await expect(page.getByText('Current Address :Ukraine, Kharkiv, Valentinivska 25a')).toBeVisible();
+    await expect(page.getByText('Permananet Address :Albania, Vlore, Ruga Gjergj Kastrioti')).toBeVisible();
 });
